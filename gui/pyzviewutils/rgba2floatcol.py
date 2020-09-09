ac277384b834555a9a336dea59f138e7c2aecdcc
Ohad Menashe <ohad.men@gmail.com>
2020-09-09 09:13:23 +0200

    trip rgba values to [0-1]

--- gui/pyzviewutils/rgba2floatcol.py
+++ gui/pyzviewutils/rgba2floatcol.py
@@ -4,8 +4,8 @@ import numpy as np
 def rgba2floatcol(rgba):
     if (rgba.shape[2] == 3):
         rgba = (np.concatenate((rgba, np.ones((list(rgba.shape[:2]) + [1]))), axis=2))
-    rgba = rgba - np.nanmin(rgba)
-    rgba = rgba / np.nanmax(rgba)
+
+    rgba = np.clip(rgba,0,1)
     rgbafloat = (rgba * 255).astype(np.uint8)
     rgbafloat = rgbafloat.flatten()
     rgbafloat = rgbafloat.view(np.float32).reshape(rgba.shape[:2])
