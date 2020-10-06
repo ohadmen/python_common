3fef0793ccb067eb81fc75743508b7468e5e3053
Ohad Menashe <ohad.men@gmail.com>
2020-10-06 23:13:18 +0200

    moved to pyzview package!

--- gui/pyzviewutils/get_trimesh_indices.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def get_trimesh_indices(sz):
-    indx = np.arange(sz[0] * sz[1],dtype=np.int32).reshape(sz[:2])
-    triA = np.stack((indx[:-1, 1:], indx[:-1, :-1], indx[1:, 1:]), axis=2).reshape(-1, 3)
-    triB = np.stack((indx[:-1, :-1], indx[1:, :-1], indx[1:, 1:]), axis=2).reshape(-1, 3)
-    tri = np.r_[triA, triB]
-    return tri
--- utils/create_transformation.py
+++ utils/create_transformation.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-from common.utils.rotation_matrix import rotatiom_matrix
+from common.utils.rotation_matrix import rotation_matrix
 
 
 def create_transformation(rot_vec, tran_vec, scale_vec=None):
     dim = len(tran_vec)
     if scale_vec is None:
         scale_vec = np.ones(dim)
-    assert len(rot_vec) == dim
+
     t = np.eye(dim + 1)
 
     if dim == 2:
@@ -15,7 +15,7 @@ def create_transformation(rot_vec, tran_vec, scale_vec=None):
         t[:dim, :dim] = np.array(((c, -s), (s, c)))
     elif dim == 3:
         assert len(rot_vec) == dim
-        t[:dim, :dim] = rotatiom_matrix(rot_vec)
+        t[:dim, :dim] = rotation_matrix(rot_vec)
     else:
         raise RuntimeError("unknown input dim {dim}")
     t[:dim, :dim] *= scale_vec
