b4065545d595efedb9b218c11231fd6e13bb93d7
Ohad Menashe <ohad.men@gmail.com>
2020-10-25 23:59:32 +0100

    rotation vector from matrix (test missing)

--- utils/rotation_matrix.py
+++ utils/rotation_matrix.py
@@ -10,11 +10,19 @@ def cpm(v):
 
 
 def rotation_matrix(rot_vec):
-    rot_vec = np.array(rot_vec)
-    angle = np.linalg.norm(rot_vec)
-    if angle==0:
-        return np.eye(3)
-    axis = rot_vec / angle
-    c = cpm(axis)
-    r = np.eye(3) + c * np.sin(angle) + (1 - np.cos(angle)) * c @ c
+    rot_vec = np.asarray(rot_vec)
+    if rot_vec.shape == (3, ):
+        angle = np.linalg.norm(rot_vec)
+        if angle == 0:
+            return np.eye(3)
+        axis = rot_vec / angle
+        c = cpm(axis)
+        r = np.eye(3) + c * np.sin(angle) + (1 - np.cos(angle)) * c @ c
+    elif rot_vec.shape == (3,3):
+        g, v = np.linalg.eig(rot_vec)
+        axis = np.real(v[:, np.argmin(np.abs(g - 1))])
+        angle = np.arccos((np.trace(rot_vec) - 1) * 0.5)
+        r = axis * angle
+    else:
+        raise RuntimeWarning("unknown input shape")
     return r
